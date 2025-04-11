b5a6508bb9e1688522451f39205648cd5cf5620c
Danyal494 <m.danyalkhan916@gmail.com>
2025-04-11 12:21:21 +0500

    down

--- app/components/PageNOne/page.jsx
+++ app/components/PageNOne/page.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const PageNOne = () => {
   return (
-    <div className='h-screen bg-purple-50'>PageFour</div>
+    <div className='h-screen bg-purple-50 text-9xl gap-8 flex flex-col items-center justify-center font-bold'>
+      Scroll Down 
+      <svg xmlns="http://www.w3.org/2000/svg" width="240" height="240" className='animate-bounce' viewBox="0 0 24 24"><path fill="#000" d="M11.178 19.569a.998.998 0 0 0 1.644 0l9-13A.999.999 0 0 0 21 5H3a1.002 1.002 0 0 0-.822 1.569z"/></svg>
+    </div>
   )
 }
 
